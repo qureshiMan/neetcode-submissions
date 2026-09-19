94c5aaf90a241cffca46717e2fd2f39f90b9f12b
Ayaan Qureshi <ayaanq787@gmail.com>
2026-09-19 13:22:32 -0700

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hasht = {}
+        hashs = {}
+
+        for i in t:
+            if i in hasht.keys():
+                hasht[i] += 1
+            else:
+                hasht[i] = 1
+        
+        for i in s:
+            if i in hashs.keys():
+                hashs[i] += 1
+            else:
+                hashs[i] = 1
+        
+        return hashs == hasht
